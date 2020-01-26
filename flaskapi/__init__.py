402c36a0bc3c7eb03671a4c19d1bd9c66b386cc7
Nicolas <nperedoesquivelgmail.com>
2020-01-26 17:48:21 -0300

    Trying to dockerize

--- flaskapi/__init__.py
+++ flaskapi/__init__.py
@@ -11,7 +11,7 @@ db = SQLAlchemy()
 
 
 def create_app():
-    app = Flask(__name__, static_folder="static", template_folder='templates')
+    app = Flask(__name__, template_folder='templates')
     Bootstrap(app)
     app.config.from_object(Config)
     db.init_app(app)
